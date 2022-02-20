57c4db83179fd768537b28b2dcc6a204b503c078
Arthurmtro <arthurmtro@gmail.com>
2022-02-20 03:19:45 +0100

    add updates

--- src/main/main.ts
+++ src/main/main.ts
@@ -12,7 +12,7 @@ export default class AppUpdater {
   constructor() {
     log.transports.file.level = 'info';
     autoUpdater.logger = log;
-    // autoUpdater.checkForUpdatesAndNotify();
+    autoUpdater.checkForUpdatesAndNotify();
   }
 }
 
